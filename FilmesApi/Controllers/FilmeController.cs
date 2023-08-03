d2edef711c843787c3a306682e0d296c99ae5d27
Bonox23 <ricardo.bonos@gmail.com>
2023-08-02 22:36:05 -0300

    Implementação do controller

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private List<Filme> filmes = new List<Filme>();
+
+        //Frombody significa que vem do corpo da requisição
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+    }
+}
